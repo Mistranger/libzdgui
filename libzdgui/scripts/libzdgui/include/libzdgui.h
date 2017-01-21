5e546cecc6cc363c71016ab3d6fb76567b411d45
cybermind <iddqd_mail@mail.ru>
2017-01-21 17:33:07 +0500

    missed file

--- libzdgui/scripts/libzdgui/include/libzdgui.h
+++ libzdgui/scripts/libzdgui/include/libzdgui.h
@@ -1,57 +1,23 @@
 #ifndef LIBZDGUI_H_INCLUDED
 #define LIBZDGUI_H_INCLUDED
-//#define ACS_SHORT_NAMES
 
-#pragma GDCC FIXED_LITERAL ON
+#include "system.h"
 
-// Includes
+#include "debug.h"
+#include "dimension.h"
+#include "event.h"
 
-#include <ACS_ZDoom.h>
-#include <assert.h>
-#include <stdarg.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdfix.h>
-#include <string.h>
-#include <limits.h>
+#include "graphics.h"
+#include "gui.h"
+#include "input.h"
+#include "widget.h"
 
-// Type definitions
+#include "widgets/button.h"
+#include "widgets/container.h"
 
-typedef __str str;
-
-// ZDoom script types
-
-#define EXT [[extern("ACS")]]
-#define SCRIPT(n) [[address(n), call("ScriptI")]]
-#define N_SCRIPT [[call("ScriptS")]]
-
-#define OPEN [[script("Open")]]
-#define ENTER [[script("Enter")]]
-#define RETURN [[script("Return")]]
-#define RESPAWN [[script("Respawn")]]
-#define DEATH [[script("Death")]]
-#define LIGHTNING [[script("Lightning")]]
-#define UNLOADING [[script("Unloading")]]
-#define DISCONNECT [[script("Disconnect")]]
-#define INPUT [[script("Input")]] // GLOOME
-
-#define NET [[script("Net")]]
-#define CLIENTSIDE [[script("Clientside")]]
-#define EVENT [[script("Event")]]
-
-__str ACS_StrParam (const char *format, ...);
-
-
-// HUDMessage
-
-int ACS_HudMessage(int flags, int id, int color, fixed x, fixed y, fixed holdTime, fixed oarg1, fixed oarg2, fixed oarg3, str format, ...);
-
-#define CharToStr(cstr) \
-  ( \
-   ACS_BeginStrParam(), \
-   __nprintf(cstr), \
-   ACS_EndStrParam() \
-  )
+#include "util/list.h"
+#include "util/queue.h"
+#include "util/stack.h"
+#include "util/util.h"
 
 #endif // LIBZDGUI_H_INCLUDED
