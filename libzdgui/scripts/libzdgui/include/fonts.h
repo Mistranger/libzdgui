bbad9a32d94fe73073d5e1aa2109547fb870c563
cybermind <iddqd_mail@mail.ru>
2017-01-23 01:04:15 +0500

    missed file

--- libzdgui/scripts/libzdgui/include/fonts.h
+++ libzdgui/scripts/libzdgui/include/fonts.h
@@ -1,7 +1,17 @@
 #ifndef FONTS_H_INCLUDED
 #define FONTS_H_INCLUDED
 
-typedef
+#include "util/string.h"
 
+typedef struct guiFont_s {
+	__str f;
+	int charWidth, charHeight;
+} guiFont_t;
+
+guiFont_t* font_new(__str font, int charWidth, int charHeight);
+int font_getWidthChar(const guiFont_t *font, const char *text);
+inline int font_getWidthString(const guiFont_t *font, const string_t *text);
+inline int font_getCharWidth(const guiFont_t *font);
+inline int font_getCharHeight(const guiFont_t *font);
 
 #endif // FONTS_H_INCLUDED
